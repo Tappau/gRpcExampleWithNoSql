25cc10af8736d4b7acc3f4857a103b5182ac57df
Calvin Tapp <calvintapp@gmail.com>
2021-03-13 23:30:13 +0000

    Initial commit

---
+++ BlazorClient/Messages/WeatherForecast.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+
+namespace Weather
+{
+    // Properties for the underlying generated data from the proto file.
+    // This partial class just adds extra properties for convenience.
+    internal partial class WeatherForecast
+    {
+        public DateTime DateTime
+        {
+            get => Date.ToDateTime();
+            set { Date= Timestamp.FromDateTime(value.ToUniversalTime());}
+        }
+
+        public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
+
+    }
+}
---
+++ BlazorClient/Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorClient
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+
+            builder.Services.AddSingleton(services =>
+            {
+                //Get the service address from appsettings.json
+                var config = services.GetRequiredService<IConfiguration>();
+                var backendUrl = config["BackendUrl"];
+
+                //if no address is set then fallback to the current webpageURL
+                if (string.IsNullOrEmpty(backendUrl))
+                {
+                    var navManager = services.GetRequiredService<NavigationManager>();
+                    backendUrl = navManager.BaseUri;
+                }
+
+                //Create a channel with a GrpcWebHandler that is connected to the backend server.
+                // GrpcWebText is used because server streaming requires it. If server streaming is not used in your app
+                // then GrpcWeb is recommended because it produces smaller messages.
+                var httpHandler = new GrpcWebHandler(GrpcWebMode.GrpcWebText, new HttpClientHandler());
+
+                return GrpcChannel.ForAddress(backendUrl, new GrpcChannelOptions {HttpHandler = httpHandler});
+            });
+
+            //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
